900b22d1f5bf309aac819f9d245c4e8537e2721b
kiennguyensys <tkien2703@gmail.com>
2023-04-03 11:21:41 +0700

    remove some goerli collections

--- constants/collection-addresses.ts
+++ constants/collection-addresses.ts
@@ -34,31 +34,7 @@ export function returnDaoAuctionContract(collectionAddress: string) {
 
 export const daoAddresses = daos.map((dao) => dao.contractAddress.toLowerCase())
 
-const collections: string[] = [
-  '0xb632fD44053B09bddDaF92dE2C212bB12Ce8DbDF',
-  '0xe169c2ed585e62b1d32615bf2591093a629549b6',
-  '0x80095419BbD6fc42701400C08119B23C1671Ad4E',
-  '0x97e54d96a1c1f6b183aa38316050ac05431857f5',
-  '0x5a79182165a2917ef9cccf33f472fe22afffeff8',
-  '0x4d2bb7d45bbe10e43ad1ba569ce85f19e85812a3',
-  '0x551e8866512183c35176752741cfd3ce9e3a6c0e',
-  '0xd9e49f550d0f605e3ccee3167ec14ee7a9134ddb',
-  '0xeef77d7f89090667f5618acc194d66d2cba42c76',
-  '0xdB05F0d43B15fba15A003B1fE7933441e04F0802',
-  '0xb9b096c521e7356888fd3cb04a3af943f401b86b',
-  '0x2605aFBb22c59296C16ef5e477110357F760b20F',
-  '0xe451fe020e8d554c10510c0572aba06eed1dcdcd',
-  '0x643AfeE78bD8C693Be049AA40e337fD15CdFB61b',
-  '0x494715b2a3c75dadd24929835b658a1c19bd4552',
-  '0x70b44ea398a33593af1cb348f3837b89a85d4f91',
-  '0x9e1f32b2BBeDe1a544b69e5403860Bba8542962B',
-]
-
 const goerliCollection: string[] = [
-  '0xd0a21d074efc3393853accd481160a070a20cf32',
-  '0x61ef03dde2429198d68e2eb18512ae14bfe5744d',
-  '0x275939bdd6052225dfe23cc895c99074ac7b1f05',
-  '0xf4729e73b5d6efc3a68f35fc2450929e0a7da19c',
   '0xc1de309d7245916c647f2830aadfa95aa75fef9e'
 ]
 
